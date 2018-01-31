7d1f02576cdde1394e4b55b2d033eb31e27284e3
Multimedia Intro <mmintro@nvc.ad.root>
2018-01-31 13:13:42 -0600

    Project2

--- project2/exsteraljs.js
+++ project2/exsteraljs.js
@@ -1,9 +1,24 @@
 // JavaScript Document
 
+
+	
+	var addn= prompt("What is 7+14");
+	switch(addn){
+		case "21":
+			alert ("Correct! You GO!");
+			break;
+			
+		default:
+			alert("Please try again.");
+	}
+
 function addnumb(a, b) {
 	var addtotal= a +b;
 	return addtotal;
 }
 
-var addnumbers2= addnumb(7, 21);
+var addnumbers2= addnumb(7, 14);
 alert (addnumbers2);
+
+
+	
